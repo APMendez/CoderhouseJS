905eb6ab58b7430636e7690d2e0306b13557f403
Agustin Mendez <agusmendez.16@gmail.com>
2022-10-23 22:43:50 -0300

    Avanzando con el DOM

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+const menu =[
+    {id:1, nombre:"Carne al horno con papas", precio: 1500 },
+    {id:2, nombre:"Lasagna de Verdura y Ricotta", precio:1400 },
+    {id:3, nombre:"Milanesa a la napolitana con papas fritas", precio:1200 },
+    {id:4, nombre:"Pizza a la Fugazzetta", precio:1600 },
+    {id:5, nombre:"Hamburguesa de lentejas", precio:950},
+    {id:6, nombre:"Falafel", precio:800},
+    {id:7, nombre:"Vino", precio:750},
+    {id:8, nombre:"Agua Mineral", precio:350},
+    {id:9, nombre:"Gaseosa", precio:450},
+    {id:10, nombre:"Cerveza", precio:600}
+]
