45c810cdc9eee532d90c5c9d44decb0e0de6a850
Agustin Mendez <agusmendez.16@gmail.com>
2022-10-24 21:06:59 -0300

    DOM y Eventos

--- js/script.js
+++ js/script.js
@@ -1,27 +1,39 @@
 let pedido = [];
-const contenedorMenu = document.getElementById("contenedorMenu");
+const contenedorMenu = document.getElementById("contenedorMenu")
 const btnOrdenar = document.getElementById("ordenar")
+const sumaTotal = document.getElementById("sumaTotal")
 
-function agregarAlPedido() {
-    console.log("agregado");
-    // const pedir = menu.find((plato)=>plato.id===opcion.id)
-    // pedido.push(pedir)
-    // console.log(pedido)
+function agregarAlPedido(opcionId) {
+    
+    const pedir = menu.find((plato)=>plato.id===opcionId)
+    pedido.push(pedir)
+    console.log(pedido)
+    sumarPedido()
+}
+function sumarPedido(){
+    let suma=0
+    for (let pedidos of pedido){
+        suma+=pedidos.precio
+    }
+    sumaTotal.innerText=suma
 }
 
 
 
 //Ejecucion del programa
+sumaTotal.innerText=0
 menu.forEach((opcion) => {
     const div = document.createElement("div");
-    div.setAttribute("id", "divMenu")
+    div.setAttribute("class", "divMenu")
     div.innerHTML = `
         <p>${opcion.nombre}</p>
         <p>$ ${opcion.precio}</p>
         <button id="btnAgregar${opcion.id}">Agregar a mi pedido</button>`
     contenedorMenu.appendChild(div)
-    const boton = document.getElementById("btnAgregar${opcion.id}")
+    const boton = document.getElementById(`btnAgregar${opcion.id}`)
     boton.addEventListener("click", () => {
-        agregarAlPedido()
+        agregarAlPedido(opcion.id)
     })
 })
+
+
